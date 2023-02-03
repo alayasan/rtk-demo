9bd18f832928e6d0f054423110fd21a537333b7c
Albert Layasan <albertlayasan01@gmail.com>
2023-02-03 13:18:27 +0800

    minor change

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,14 +3,14 @@ import cakeReducer from '../features/cake/cakeSlice'
 import iceCreamReducer from '../features/iceCream/iceCreamSlice'
 import { createLogger } from 'redux-logger';
 
-const logger = createLogger()
+const xlogger = createLogger()
 
 const store = configureStore({
   reducer: {
     cake: cakeReducer,
     iceCream: iceCreamReducer,
   },
-  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), logger],
+  middleware: (getDefaultMiddleware) => [...getDefaultMiddleware(), xlogger],
 })
 
 export default store
